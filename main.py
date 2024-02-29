92a43d89c6bee32f90dcf5a5d9d15155c21b0e17
iNaneek <118861082+iNaneek@users.noreply.github.com>
2024-02-29 09:09:50 -0700

    Made two sides, no overlap control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 
 quadrantLen = 500
 
@@ -8,59 +9,10 @@ pygame.display.set_caption("pygame") #defines the windows name
 
 
 
-class Point:
 
-    def __init__(self, pos = [10, 10, 10]):
-        self.pos = pos
-        #pos = [x, y, z]
-        #x = l/f
-        #y = in/out
-        #z = u/d
 
-    def returnAngle(self, axis = 0):
-        return math.degrees(math.atan(self.pos[axis]/self.pos[1]))
 
-    def move(self, dir):
-        if dir == 0:
-            self.pos[0] -= 0.1
-        if dir == 1:
-            self.pos[0] += 0.1
-        if dir == 2:
-            self.pos[2] -= 0.1
-        if dir == 3:
-            self.pos[2] += 0.1
-
-class Line:
-
-    def __init__(self, pos = [10, 10, 10], pos2 = [20, 20, 20]):
-        self.pos = pos
-        self.pos2 = pos2
-        #pos = [x, y, z]
-        #x = l/f
-        #y = in/out
-        #z = u/d
-
-    def returnPoint1Coords(self):
-        return (500 + math.degrees(math.atan(self.pos[0]/self.pos[1])) * 15, 500 - math.degrees(math.atan(self.pos[2]/self.pos[1])) * 15)
-
-    def returnPoint2Coords(self):
-        return (500 + math.degrees(math.atan(self.pos2[0]/self.pos2[1])) * 15, 500 - math.degrees(math.atan(self.pos2[2]/self.pos2[1])) * 15)
-
-    def move(self, dir):
-        if dir == 0:
-            self.pos[0] -= 0.1
-            self.pos2[0] -= 0.1
-        if dir == 1:
-            self.pos[0] += 0.1
-            self.pos2[0] += 0.1
-        if dir == 2:
-            self.pos[2] -= 0.1
-            self.pos2[2] -= 0.1
-        if dir == 3:
-            self.pos[2] += 0.1
-            self.pos2[2] += 0.1
-
-class Cubeoid():
+class Cuboid():
     def __init__(self, pos = [10, 10, 10], radius = 2, angle = math.pi/4):
         self.pos = pos
         self.radius = radius
@@ -69,24 +21,23 @@ class Cubeoid():
         self.angle = angle
 
     def findPoints(self):
-        self.points = {}
-        self.points[int(len(self.points))] = Point([self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi), self.pos[2] + self.radius])
-        self.points[int(len(self.points))] = Point([self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi), self.pos[2] - self.radius])
-        self.points[int(len(self.points))] = Point([self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi * 1.5), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi * 1.5), self.pos[2] + self.radius])
-        self.points[int(len(self.points))] = Point([self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi * 1.5), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi * 1.5), self.pos[2] - self.radius])
+        self.points = (
+        [self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi), self.pos[2] + self.radius],
+        [self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi), self.pos[2] - self.radius],
+        [self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi * 1.5), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi * 1.5), self.pos[2] + self.radius],
+        [self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi * 1.5), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi * 1.5), self.pos[2] - self.radius],
 
-        self.points[int(len(self.points))] = Point([self.pos[0] + self.adjustedRadius * math.cos(self.angle), self.pos[1] + self.adjustedRadius * math.sin(self.angle), self.pos[2] + self.radius])
-        self.points[int(len(self.points))] = Point([self.pos[0] + self.adjustedRadius * math.cos(self.angle), self.pos[1] + self.adjustedRadius * math.sin(self.angle), self.pos[2] - self.radius])
-        self.points[int(len(self.points))] = Point([self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi * 0.5), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi * 0.5), self.pos[2] + self.radius])
-        self.points[int(len(self.points))] = Point([self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi * 0.5), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi * 0.5), self.pos[2] - self.radius])
-
+        [self.pos[0] + self.adjustedRadius * math.cos(self.angle), self.pos[1] + self.adjustedRadius * math.sin(self.angle), self.pos[2] + self.radius],
+        [self.pos[0] + self.adjustedRadius * math.cos(self.angle), self.pos[1] + self.adjustedRadius * math.sin(self.angle), self.pos[2] - self.radius],
+        [self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi * 0.5), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi * 0.5), self.pos[2] + self.radius],
+        [self.pos[0] + self.adjustedRadius * math.cos(self.angle + math.pi * 0.5), self.pos[1] + self.adjustedRadius * math.sin(self.angle + math.pi * 0.5), self.pos[2] - self.radius]
+        )
+        
     def drawPoints(self):
-        for index in list(self.points):
+        for index in range(len(self.points)):
             pygame.draw.circle(win, (255, 255, 255),
-                               (500 + self.points[index].returnAngle(0) * 15, 500 - self.points[index].returnAngle(2) * 15), 3)
-    def drawLines(self):
-
-        rectangle_edges = (
+                               (500 + math.degrees(math.atan(self.points[index][0]/self.points[index][1])) * 15, 500 - math.degrees(math.atan(self.points[index][2]/self.points[index][1])) * 15), 3)
+    rectangle_edges = (
             (0, 2),
             (2, 4),
             (4, 6),
@@ -100,14 +51,42 @@ class Cubeoid():
             (4, 5),
             (6, 7),
         )
-        for edge in rectangle_edges:
+    
+    def drawLines(self):
+
+        
+        for edge in self.rectangle_edges:
             pygame.draw.line(win, (255, 255, 255),
-                             (quadrantLen + math.degrees(math.atan(self.points[edge[0]].pos[0] / self.points[edge[0]].pos[1])) * 15,
-                              quadrantLen - math.degrees(math.atan(self.points[edge[0]].pos[2] / self.points[edge[0]].pos[1])) * 15),
-                             (quadrantLen + math.degrees(math.atan(self.points[edge[1]].pos[0] / self.points[edge[1]].pos[1])) * 15,
-                              quadrantLen - math.degrees(math.atan(self.points[edge[1]].pos[2] / self.points[edge[1]].pos[1])) * 15))
+                             (quadrantLen + math.degrees(math.atan(self.points[edge[0]][0] / self.points[edge[0]][1])) * 15,
+                              quadrantLen - math.degrees(math.atan(self.points[edge[0]][2] / self.points[edge[0]][1])) * 15),
+                             (quadrantLen + math.degrees(math.atan(self.points[edge[1]][0] / self.points[edge[1]][1])) * 15,
+                              quadrantLen - math.degrees(math.atan(self.points[edge[1]][2] / self.points[edge[1]][1])) * 15))
+    cuboidSides = (
+        (0, 1, 3, 2),
+        (0, 2, 4, 6)
+    )
+    def drawSides(self):
+        for side in self.cuboidSides:
+            pygame.draw.polygon(win, (random.randint(0,255), random.randint(0,255), random.randint(0,255)), 
+
+                             (
+                             (quadrantLen + math.degrees(math.atan(self.points[side[0]][0] / self.points[side[0]][1])) * 15,
+                              quadrantLen - math.degrees(math.atan(self.points[side[0]][2] / self.points[side[0]][1])) * 15),
+                             (quadrantLen + math.degrees(math.atan(self.points[side[1]][0] / self.points[side[1]][1])) * 15,
+                              quadrantLen - math.degrees(math.atan(self.points[side[1]][2] / self.points[side[1]][1])) * 15),
+                             (quadrantLen + math.degrees(math.atan(self.points[side[2]][0] / self.points[side[2]][1])) * 15,
+                              quadrantLen - math.degrees(math.atan(self.points[side[2]][2] / self.points[side[2]][1])) * 15),
+                             (quadrantLen + math.degrees(math.atan(self.points[side[3]][0] / self.points[side[3]][1])) * 15,
+                              quadrantLen - math.degrees(math.atan(self.points[side[3]][2] / self.points[side[3]][1])) * 15)))
+            '''
+        pygame.draw.circle(win, (255, 0, 0),
+                         (quadrantLen + math.degrees(math.atan(self.points[0][0] / self.points[0][1])) * 15,
+                          quadrantLen - math.degrees(math.atan(self.points[0][2] / self.points[0][1])) * 15), 0,
+                          width = 10)
+                          '''
 
     def move(self, dir):
+        self.findPoints()
         if dir == 0:
             self.pos[0] -= 0.1
         if dir == 1:
@@ -125,19 +104,21 @@ class Cubeoid():
 
 
 
-#c1 = Cubeoid([5, 15, 5], 2)
-#c2 = Cubeoid([5, 15, 5], 1)
+#c1 = Cuboid([5, 15, 5], 2)
+#c2 = Cuboid([5, 15, 5], 1)
 
 cubes = {}
-cubes[int(len(cubes))] = Cubeoid([5, 15, 5], 2)
-cubes[int(len(cubes))] = Cubeoid([5, 15, 5], 1)
+#cubes[int(len(cubes))] = Cuboid([5, 15, 5], 2)
+#cubes[int(len(cubes))] = Cuboid([5, 15, 5], 1)
 
-'''
-for i in range(16000):
-    cubes[int(len(cubes))] = Cubeoid([5, 15, 5], i/16000)
+#'''
+for j in range(10):
+    for i in range(10):
+        cubes[int(len(cubes))] = Cuboid([j*2 - 10, 15, i*2 - 10], 0.5)
 #'''
 
-
+for index in cubes:
+        cubes[index].findPoints()
 
 
 x = 0
@@ -186,9 +167,10 @@ while running:
             cubes[index].move(6)
 
     for index in cubes:
-        cubes[index].findPoints()
+        #cubes[index].findPoints()
         cubes[index].drawPoints()
         cubes[index].drawLines()
+        cubes[index].drawSides()
 
 
 
