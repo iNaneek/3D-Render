d2f01415a432ee9ecc5c36af02773b0df04be9d9
iNaneek <118861082+iNaneek@users.noreply.github.com>
2024-03-12 09:09:38 -0600

    Spinning wheels

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 #presets
 #----------------------------------------------------
 array_of_cubes = False
-single_cube = True
+single_cube = False
 #----------------------------------------------------
 
 
@@ -33,6 +33,7 @@ class Cuboid:
             (255, 255, 0),
             (255, 255, 255)
         )
+        print('hello')
 
     def find_points(self):
         self.points = (
@@ -149,8 +150,8 @@ class Cuboid:
             self.angle += 0.1
 
 
-# c1 = Cuboid([5, 15, 5], 2)
-# c2 = Cuboid([5, 15, 5], 1)
+c1 = Cuboid([5, 15, 5], 1)
+c2 = Cuboid([5, 15, 5], 1)
 
 cubes = {}
 if single_cube:
@@ -162,15 +163,81 @@ if array_of_cubes:
         for i in range(10):
             cubes[int(len(cubes))] = Cuboid([j*2 - 10, 15, i*2 - 10], 0.5)
 
+cubes1 = {}
+cubes2 = {}
+cubes3 = {}
+cubes4 = {}
 
-for index in cubes:
-    cubes[index].find_points()
-    cubes[index].find_side_positions()
+for i in range(10):
+    cubes1[int(len(cubes1))] = Cuboid([5, 15, 5], 1)
 
+for i in range(10):
+    cubes2[int(len(cubes2))] = Cuboid([5, 15, 5], 1)
+
+for i in range(10):
+    cubes3[int(len(cubes3))] = Cuboid([5, 15, 5], 1)
+
+for i in range(10):
+    cubes4[int(len(cubes4))] = Cuboid([5, 15, 5], 1)
+
+for index in cubes1:
+    cubes1[index].find_points()
+    cubes1[index].find_side_positions()
+    cubes2[index].find_points()
+    cubes2[index].find_side_positions()
+    cubes3[index].find_points()
+    cubes3[index].find_side_positions()
+
+cubes1[0].colors = ((250, 20, 20), (150, 50, 50), (250, 20, 20), (150, 50, 50), (255, 0, 0), (100, 0, 0))
+cubes1[1].colors = ((250, 250, 250), (240, 240, 240), (250, 250, 250), (240, 240, 240),  (255, 255, 255), (200, 200, 200))
+cubes1[2].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes1[3].colors = ((20, 250, 20), (50, 150, 50), (20, 250, 20), (50, 150, 50), (0, 255, 0), (0, 100, 0))
+cubes1[4].colors = ((200, 200, 0), (170, 170, 0), (200, 200, 0), (170, 170, 0), (255, 255, 0), (150, 150, 0))
+cubes1[5].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes1[6].colors = ((250, 250, 250), (240, 240, 240), (250, 250, 250), (240, 240, 240),  (255, 255, 255), (200, 200, 200))
+cubes1[7].colors = ((20, 250, 20), (50, 150, 50), (20, 250, 20), (50, 150, 50), (0, 255, 0), (0, 100, 0))
+cubes1[8].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes1[9].colors = ((200, 200, 0), (170, 170, 0), (200, 200, 0), (170, 170, 0), (255, 255, 0), (150, 150, 0))
+
+cubes2[0].colors = ((250, 20, 20), (150, 50, 50), (250, 20, 20), (150, 50, 50), (255, 0, 0), (100, 0, 0))
+cubes2[1].colors = ((250, 250, 250), (240, 240, 240), (250, 250, 250), (240, 240, 240),  (255, 255, 255), (200, 200, 200))
+cubes2[2].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes2[3].colors = ((20, 250, 20), (50, 150, 50), (20, 250, 20), (50, 150, 50), (0, 255, 0), (0, 100, 0))
+cubes2[4].colors = ((200, 200, 0), (170, 170, 0), (200, 200, 0), (170, 170, 0), (255, 255, 0), (150, 150, 0))
+cubes2[5].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes2[6].colors = ((250, 250, 250), (240, 240, 240), (250, 250, 250), (240, 240, 240),  (255, 255, 255), (200, 200, 200))
+cubes2[7].colors = ((20, 250, 20), (50, 150, 50), (20, 250, 20), (50, 150, 50), (0, 255, 0), (0, 100, 0))
+cubes2[8].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes2[9].colors = ((200, 200, 0), (170, 170, 0), (200, 200, 0), (170, 170, 0), (255, 255, 0), (150, 150, 0))
+
+cubes3[0].colors = ((250, 20, 20), (150, 50, 50), (250, 20, 20), (150, 50, 50), (255, 0, 0), (100, 0, 0))
+cubes3[1].colors = ((250, 250, 250), (240, 240, 240), (250, 250, 250), (240, 240, 240),  (255, 255, 255), (200, 200, 200))
+cubes3[2].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes3[3].colors = ((20, 250, 20), (50, 150, 50), (20, 250, 20), (50, 150, 50), (0, 255, 0), (0, 100, 0))
+cubes3[4].colors = ((200, 200, 0), (170, 170, 0), (200, 200, 0), (170, 170, 0), (255, 255, 0), (150, 150, 0))
+cubes3[5].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes3[6].colors = ((250, 250, 250), (240, 240, 240), (250, 250, 250), (240, 240, 240),  (255, 255, 255), (200, 200, 200))
+cubes3[7].colors = ((20, 250, 20), (50, 150, 50), (20, 250, 20), (50, 150, 50), (0, 255, 0), (0, 100, 0))
+cubes3[8].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes3[9].colors = ((200, 200, 0), (170, 170, 0), (200, 200, 0), (170, 170, 0), (255, 255, 0), (150, 150, 0))
+
+cubes4[0].colors = ((250, 20, 20), (150, 50, 50), (250, 20, 20), (150, 50, 50), (255, 0, 0), (100, 0, 0))
+cubes4[1].colors = ((250, 250, 250), (240, 240, 240), (250, 250, 250), (240, 240, 240),  (255, 255, 255), (200, 200, 200))
+cubes4[2].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes4[3].colors = ((20, 250, 20), (50, 150, 50), (20, 250, 20), (50, 150, 50), (0, 255, 0), (0, 100, 0))
+cubes4[4].colors = ((200, 200, 0), (170, 170, 0), (200, 200, 0), (170, 170, 0), (255, 255, 0), (150, 150, 0))
+cubes4[5].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes4[6].colors = ((250, 250, 250), (240, 240, 240), (250, 250, 250), (240, 240, 240),  (255, 255, 255), (200, 200, 200))
+cubes4[7].colors = ((20, 250, 20), (50, 150, 50), (20, 250, 20), (50, 150, 50), (0, 255, 0), (0, 100, 0))
+cubes4[8].colors = ((20, 20, 250), (50, 50, 150), (20, 20, 250), (50, 50, 150), (0, 0, 255), (0, 0, 100))
+cubes4[9].colors = ((200, 200, 0), (170, 170, 0), (200, 200, 0), (170, 170, 0), (255, 255, 0), (150, 150, 0))
+
+speed = [10, 10, 10, 10]
+pos = [0, 0, 0, 0]
 x = 0
 running = True
 while running:
-    pygame.time.delay(20)  # time between frames (in ms)
+    #pygame.time.delay(2)  # time between frames (in ms)
     for event in pygame.event.get():  # when close button pushed, loop ends
         if event.type == pygame.QUIT:
             running = False
@@ -181,45 +248,90 @@ while running:
 
     pygame.draw.circle(win, (255, 0, 0), (500, 500), 3)
 
-    x += 1
-    #print(x)
+    speed[0] -= 1
+    speed[1] -= 0.6
+    speed[2] -= 0.3
+    speed[3] -= 0.1
+
+    pos[0] += speed[0]
+    pos[1] += speed[1]
+    pos[2] += speed[2]
+    pos[3] += speed[3]
+
+    x += 0.5
+    print(x)
     if x == 10000:
         print(time.time()-t1)
         pass
 
     if keys[pygame.K_a]:
-        for index in cubes:
-            cubes[index].move(0)
+        for index in cubes1:
+            cubes1[index].move(0)
 
     if keys[pygame.K_d]:
-        for index in cubes:
-            cubes[index].move(1)
+        for index in cubes1:
+            cubes1[index].move(1)
 
     if keys[pygame.K_s]:
-        for index in cubes:
-            cubes[index].move(2)
+        for index in cubes1:
+            cubes1[index].move(2)
 
     if keys[pygame.K_w]:
-        for index in cubes:
-            cubes[index].move(3)
+        for index in cubes1:
+            cubes1[index].move(3)
 
     if keys[pygame.K_j]:
-        for index in cubes:
-            cubes[index].move(4)
+        for index in cubes1:
+            cubes1[index].move(4)
 
     if keys[pygame.K_k]:
-        for index in cubes:
-            cubes[index].move(5)
+        for index in cubes1:
+            cubes1[index].move(5)
 
     if keys[pygame.K_f]:
-        for index in cubes:
-            cubes[index].move(6)
+        for index in cubes1:
+            cubes1[index].move(6)
 
-    for index in cubes:
-        # cubes[index].findPoints()
-        #cubes[index].draw_points()
-        #cubes[index].draw_lines()
-        cubes[index].draw_sides()
+    #rwbgybwgby
+    
+    
+
+
+    cubes1 = dict(sorted(cubes1.items(), key=lambda item: item[1].pos[1], reverse=True))
+
+    for indx in cubes1:
+        cubes1[indx].pos = [4.5, math.cos(indx * math.pi/5 + x/30)*5+15, math.sin(indx * math.pi/5 + x/30)*5]
+        cubes1[indx].find_points()
+        cubes1[indx].find_side_positions()
+        cubes1[indx].draw_sides()
+        #cubes[index].move(6)
+    
+    cubes2 = dict(sorted(cubes1.items(), key=lambda item: item[1].pos[1], reverse=True))
+
+    for indx in cubes2:
+        cubes2[indx].pos = [-4.5, math.cos(indx * math.pi/5 + x/30)*5+15, math.sin(indx * math.pi/5 + x/30)*5]
+        cubes2[indx].find_points()
+        cubes2[indx].find_side_positions()
+        cubes2[indx].draw_sides()
+        #cubes[index].move(6)
+    
+    cubes3 = dict(sorted(cubes1.items(), key=lambda item: item[1].pos[1], reverse=True))
+
+    for indx in cubes3:
+        cubes3[indx].pos = [1.5, math.cos(indx * math.pi/5 + x/30)*5+15, math.sin(indx * math.pi/5 + x/30)*5]
+        cubes3[indx].find_points()
+        cubes3[indx].find_side_positions()
+        cubes3[indx].draw_sides()
+        #cubes[index].move(6)
+    
+    cubes4 = dict(sorted(cubes1.items(), key=lambda item: item[1].pos[1], reverse=True))
+
+    for indx in cubes4:
+        cubes4[indx].pos = [-1.5, math.cos(indx * math.pi/5 + x/30)*5+15, math.sin(indx * math.pi/5 + x/30)*5]
+        cubes4[indx].find_points()
+        cubes4[indx].find_side_positions()
+        cubes4[indx].draw_sides()
+        #cubes[index].move(6)
 
     pygame.display.update()
 pygame.quit()  # when the loop ends it closes the window
